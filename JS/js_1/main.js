a091d8741b1380c74286b2dac4830efea5f53584
macbook <ivan.kalnytskiy@gmail.com>
2021-03-23 11:54:42 +0200

    js_1 home-work-1

---
+++ JS/js_1/main.js
@@ -0,0 +1,173 @@
+//-----------------------------------------task-1-------------------------------------//
+
+/*let str0='hello';
+let str1='owu';
+let str2='com';
+let str3='ua';
+let number1=1;
+let number2=10;
+let number3=-999;
+let number4=123;
+let numberPi=3.14;
+let number5=2.7;
+let number6=16;
+let active=true;
+let notActive=false;
+
+console.log(str0,str1,str2,str3,number1,number2,number3,number4,number5,number6,numberPi,notActive,active);
+alert(str0);alert(str1);alert(str2);alert(str3);alert(number1);alert(number2);alert(number3);alert(number4);alert(number5);alert(number6);alert(numberPi);alert(notActive);alert(active);
+document.write(`<h3>${str0},${str1},${str2},${str3},${number1},${number2},${number3},${number4},${number5},${number6},${numberPi},${active},${notActive}</h3>`);*/
+/*document.write('<hr>');*/
+//-----------------------------------------task-2-------------------------------------//
+/*let str0='hi';
+let str1='okt';
+let str2='comp';
+let str3='uar';
+let number1=3;
+let number2=589;
+let number3=-23;
+let number4=422;
+let numberPi=1.6;
+let number5=5.9;
+let number6=19;
+let active=false;
+let notActive=true;
+
+console.log(str0,str1,str2,str3,number1,number2,number3,number4,number5,number6,numberPi,notActive,active);
+alert(str0);alert(str1);alert(str2);alert(str3);alert(number1);alert(number2);alert(number3);alert(number4);alert(number5);alert(number6);alert(numberPi);alert(notActive);alert(active);
+document.write(`<h3>${str0},${str1},${str2},${str3},${number1},${number2},${number3},${number4},${number5},${number6},${numberPi},${active},${notActive}</h3>`);*/
+/*document.write('<hr>');*/
+//-----------------------------------------task-3-------------------------------------//
+/*const age=20;
+const Pi=3.14;
+const year=2021;
+const name='Ivan';
+const skils="JS";
+const compani='OKT';
+
+console.log(age,Pi,year,name,skils,compani);
+alert(age);alert(Pi);alert(year);alert(name);alert(skils);alert(compani);
+document.write(`<h3>${age},${Pi},${year},${name},${skils},${compani}</h3>`)*/
+/*document.write('<hr>');*/
+//-----------------------------------------task-4,5-------------------------------------//
+/*let name=prompt("name-");
+let SurName=prompt("SurName-");
+let PobName=prompt("PobName-");
+
+console.log(name,SurName,PobName);
+alert(name);alert(SurName);alert(PobName);
+document.write(`<h3>${name},${SurName},${PobName}</h3>`)
+let person=(name+' '+SurName+' '+PobName);//TASK5
+console.log(person);
+document.write('<hr>');*/
+//-----------------------------------------task-6-------------------------------------//
+/*let name=prompt("name-");
+let SurName=prompt("SurName-");
+let PobName=prompt("PobName-");
+
+console.log(name,SurName,PobName);
+alert(name);alert(SurName);alert(PobName);
+document.write(`<h3>${name},${SurName},${PobName}</h3>`)
+let person=(name+' '+SurName+' '+PobName);//TASK5
+console.log(person);
+
+let nameMather=prompt("nameMather-");
+let SurNameMather=prompt("SurNameMather-");
+let PobNameMather=prompt("PobNameMather-");
+
+console.log(nameMather,SurNameMather,PobNameMather);
+alert(nameMather);alert(SurNameMather);alert(PobNameMather);
+document.write(`<h3>${nameMather},${SurNameMather},${PobNameMather}</h3>`)
+let personMather=(nameMather+' '+SurNameMather+' '+PobNameMather);//TASK5
+console.log(personMather);
+
+let nameBrother=prompt("nameBrother-");
+let SurNameBrother=prompt("SurNameBrother-");
+let PobNameBrother=prompt("PobNameBrother-");
+
+console.log(nameBrother,SurNameBrother,PobNameBrother);
+alert(nameBrother);alert(SurNameBrother);alert(PobNameBrother);
+document.write(`<h3>${nameBrother},${SurNameBrother},${PobNameBrother}</h3>`)
+let personBrother=(nameBrother+' '+SurNameBrother+' '+PobNameBrother);//TASK5
+console.log(personBrother);*/
+//-----------------------------------------task-7-------------------------------------//
+/*
+let  number=+prompt("number-");
+let  number2=+prompt("number2-");
+let  number3=+prompt("number3-");
+console.log(number);
+console.log(number2);
+console.log(number3);*/
+//-----------------------------------------task-8-------------------------------------//
+/*let  number=parseInt(prompt("number-"));
+let  number2=parseInt(prompt("number2-"));
+let  number3=parseInt(prompt("number3-"));
+let  number4=parseInt(prompt("number4-"));
+
+let result=number+number2+number3+number4;
+console.log(result);*/
+//-----------------------------------------task-9-------------------------------------//
+/*
+let  number=parseFloat(prompt("number-"));
+let  number2=parseFloat(prompt("number2-"));
+let  number3=parseFloat(prompt("number3-"));
+
+let result=number+number2+number3;
+console.log(parseFloat(result));*/
+//-----------------------------------------task-10,11 - ще не таке не вчили-------------------------------------//
+
+//-----------------------------------------task-12-------------------------------------//
+/*
+let a = 100;
+let b = '100';
+let c = true;
+let d = undefined;
+
+console.log(typeof a,typeof b,typeof c,typeof d,);*/
+//-----------------------------------------task-12-------------------------------------//
+
+/*
+let value=(5<6);
+console.log(value);
+let value1=(5>6);
+console.log(value1);
+let value2=(5>=6);
+console.log(value2);
+let value3=(5===6);
+console.log(value3);
+let value4=(10===10);
+console.log(value4);
+let value5=(10==10);
+console.log(value5);
+let value6=(10 >10);
+console.log(value6);
+let value7=(10 < 10);
+console.log(value7);
+let value8=(10 != 10);
+console.log(value8);
+let value9=(123 ==='123');
+console.log(value9);
+let value10=(123 == '123');
+console.log(value10);*/
+
+/*
+Дополнительно:
+ - Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+console.log(132 > 100 && 45 < 12 ); false
+console.log(34 > 33 && 23 < 90 ); true
+console.log(99 > 100 && 45 > 12 ); false
+console.log(132 > 100 || 45 < 12 ); true
+console.log(111 > 11 || 45 < 111 ); true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); true
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); true
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); false
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); true
+console.log(!!'-1');true
+console.log(!!-1);true
+console.log(!!'0');true
+console.log(!!'null');true
+console.log(!!'undefined');true
+console.log(!!(3/'owu'));false
+console.log((111 > 11 || 45 < 111) ||  !!'0');true
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));false
+*/
